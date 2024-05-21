8509f319a421b48174a81c26af0d2c600ac6d239
FhantomDev <cribillerodev@gmail.com>
2024-05-21 15:14:32 -0400

    Generar nuemro random para los id de la compra

--- Ferremax/static/js/pedido.js
+++ Ferremax/static/js/pedido.js
@@ -3,5 +3,22 @@ document.addEventListener('DOMContentLoaded', () => {
     const total = localStorage.getItem('total-compra')
     const pesos = document.querySelector('#pesos')
     pesos.textContent = total;
-
+    numerosRandom()
 })
+
+function numerosRandom() {
+    const ordenTxt = document.querySelector('#orden_txt')
+    const ordenV = document.querySelector('#orden_value')
+    const sesionTxt = document.querySelector('#sesion_txt')
+    const sesionV = document.querySelector('#sesion_value')
+
+    let randomOrden = Math.floor(Math.random() * (90000000 - 10000000 + 1)) + 10000000;
+    console.log(randomOrden)
+    ordenTxt.textContent = randomOrden;
+    ordenV.value = randomOrden;
+
+    let randomSesion = Math.floor(Math.random() * (90000000 - 10000000 + 1)) + 10000000;
+    console.log(randomSesion)
+    sesionTxt.textContent = randomSesion;
+    sesionV.value = randomSesion;
+}
